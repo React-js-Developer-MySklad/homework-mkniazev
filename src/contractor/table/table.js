535a0ba3fe5c0ef6aae11cc158573e7ac93e64f2
mkniazev <mkniazev@moysklad.ru>
2024-08-05 13:10:50 +0500

    реализовать отображение таблицы на основании массива данных. Колонки: наименование, ИНН, адрес, КПП, кнопка удалить.

--- src/contractor/table/table.js
+++ src/contractor/table/table.js
@@ -10,7 +10,11 @@ const tableHeadElement = document.getElementById("table-head");
 tableHeadElement.classList.add("text-xs", "text-gray-600", "uppercase");
 tableHeadElement.appendChild(createTableHead());
 
-//TODO add same for body
+const tableBodyElement = document.getElementById("table-body");
+tableBodyElement.classList.add("text-sm");
+getAppData().forEach(data => {
+    tableBodyElement.appendChild(createTableRow(data));
+});
 
 function createTableHead() {
     const tr = document.createElement("tr");
@@ -20,17 +24,55 @@ function createTableHead() {
     tr.appendChild(createTableHeadData("ИНН"));
     tr.appendChild(createTableHeadData("Адрес"));
     tr.appendChild(createTableHeadData("КПП"));
-    tr.appendChild(createTableHeadData("")); // trachcan
+    tr.appendChild(createTableHeadData("")); // trash can
 
     return tr;
 }
 
-function createTableHeadData(value) {
-    const td  = document.createElement("td");
+function createTableHeadData(columnName) {
+    const td = document.createElement("td");
     td.scope = "col";
     td.classList.add("px-4", "py-4", "p-1");
-    td.innerHTML = value;
+    td.innerHTML = columnName;
 
     return td;
 }
 
+function createTableRow(data) {
+    console.log(data)
+    const tr = document.createElement("tr");
+    tr.classList.add("border-b", "border-gray-200");
+
+    const name = document.createElement("td");
+    name.classList.add("px-4", "py-4", "text-gray-900");
+    name.innerHTML = data.name;
+
+    const inn = document.createElement("td");
+    inn.classList.add("px-4", "py-4", "text-gray-500");
+    inn.innerHTML = data.inn;
+
+    const address = document.createElement("td");
+    address.classList.add("px-4", "py-4", "text-gray-900");
+    address.innerHTML = data.address;
+
+    const kpp = document.createElement("td");
+    kpp.classList.add("px-4", "py-4", "text-gray-500");
+    kpp.innerHTML = data.kpp;
+
+    const deletion = document.createElement("td");
+    deletion.classList.add("px-4", "py-4", "text-right");
+    const deleteButton = document.createElement("a");
+    deleteButton.classList.add("font-medium", "text-blue-600", "hover:underline");
+    deleteButton.href = "#";
+    deleteButton.innerHTML = "Удалить";
+
+    deletion.appendChild(deleteButton)
+
+
+    tr.appendChild(name);
+    tr.appendChild(inn);
+    tr.appendChild(address);
+    tr.appendChild(kpp);
+    tr.appendChild(deletion);
+    return tr;
+}
