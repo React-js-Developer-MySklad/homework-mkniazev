a044c6b2d570edd4afeea1003937cad41c8859fd
Maksim Kniazev <34174842+knzv-dev@users.noreply.github.com>
2024-09-09 18:36:31 +0500

    final-forms валидация (#6)
    Co-authored-by: mkniazev <mkniazev@moysklad.ru>

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,24 +1,50 @@
 import React from 'react'
+import { Form, Field } from 'react-final-form'
 
 export default function Modal(props) {
 
-    const onNameChange = (e) => {
-        props.setModalData({...props.data, name: e.target.value})
+    const onSubmitModal = (e) => {
+        console.log(e)
+        props.onSubmitModal({ ...props.data, 
+            name: e.name,
+            inn: e.inn,
+            address: e.address,
+            kpp: e.kpp });
     }
 
-    const onInnChange = (e) => {
-        props.setModalData({...props.data, inn: e.target.value})
+    const validate = (values) => {
+        const errors = {}
+
+        
+        if (!values.name) {
+            errors.name = 'Поле обязательно к заполнению'
+        }
+
+
+        if (!values.inn) {
+            errors.inn = 'Поле обязательно к заполнению'
+        } else if (isNaN(values.inn)){
+            errors.inn = "ИНН должен быть числом"
+        } else if (values.inn.length != 10 && values.inn.length != 12) {
+            errors.inn = "ИНН должен содержать 10 или 12 симоволов"
+        }
+
+        if (!values.address) {
+            errors.address = 'Поле обязательно к заполнению'
+        }
+
+        if (!values.kpp) {
+            errors.kpp = 'Поле обязательно к заполнению'
+        } else if (isNaN(values.kpp)){
+            errors.kpp = "КПП должен быть числом"
+        } else if (values.kpp.length != 9) {
+            errors.kpp = "КПП должен содержать 9 симоволов"
+        }
+
+        return errors;
     }
 
-    const onAddressChange = (e) => {
-        props.setModalData({...props.data, address: e.target.value})
-    }
 
-    const onKppChange = (e) => {
-        props.setModalData({...props.data, kpp: e.target.value})
-    }
-
-    
     return (
         <div id="add-data-modal" tabIndex="-1" aria-hidden="true" className={`${props.isModalOpen ? "backdrop-blur" : "hidden"} overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full`}>
             <div className="p-4 w-full max-w-md max-h-full absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
@@ -35,28 +61,72 @@ export default function Modal(props) {
                         </button>
                     </div>
                     <div className="p-4 md:p-5">
-                        <form className="space-y-4" name="addContractorForm">
-                            <div>
-                                <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900">Наименование</label>
-                                <input type="text" name="name" id="name" onChange={onNameChange} value={props.data.name} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required/>
-                            </div>
-                            <div>
-                                <label htmlFor="inn" className="block mb-2 text-sm font-medium text-gray-900">ИНН</label>
-                                <input type="text" name="inn" id="inn" onChange={onInnChange} value={props.data.inn} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required />
-                            </div>
-                            <div>
-                                <label htmlFor="address" className="block mb-2 text-sm font-medium text-gray-900">Адрес</label>
-                                <input type="text" name="address" id="address" onChange={onAddressChange} value={props.data.address} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required />
-                            </div>
-                            <div>
-                                <label htmlFor="kpp" className="block mb-2 text-sm font-medium text-gray-900">КПП</label>
-                                <input type="text" name="kpp" id="kpp" onChange={onKppChange} value={props.data.kpp} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required />
-                            </div>
-                            <div className="w-full flex items-center justify-between p-4 md:p-5 border-b rounded-t">
-                                <button type="button" onClick={props.onSubmitModal} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Сохранить</button>
-                                <button type="button" onClick={props.onCloseModal} className="text-white bg-red-500 hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Отменить</button>
-                            </div>
-                        </form>
+                        <Form
+                            onSubmit={onSubmitModal}
+                            validate={validate}
+                            initialValues={{
+                                name: props.data.name,
+                                inn: props.data.inn,
+                                address: props.data.address,
+                                kpp: props.data.kpp
+                            }}
+                            render={
+                                ({ handleSubmit }) => (
+                                    <form className="space-y-4" name="addContractorForm" onSubmit={handleSubmit}>
+                                        <div>
+
+                                            <Field name="name">
+                                                {({ input, meta }) => (
+                                                    <div>
+                                                        <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900">Наименование</label>
+                                                        <input {...input} type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" />
+                                                        {meta.error && meta.touched && <span className="text-sm text-red-500">{meta.error}</span>}
+                                                    </div>
+                                                )}
+                                            </Field>
+
+                                        </div>
+                                        <div>
+                                            <Field name="inn">
+                                                {({ input, meta }) => (
+                                                    <div>
+                                                        <label htmlFor="inn" className="block mb-2 text-sm font-medium text-gray-900">ИНН</label>
+                                                        <input {...input} type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" />
+                                                        {meta.error && meta.touched && <span className="text-sm text-red-500">{meta.error}</span>}
+                                                    </div>
+                                                )}
+                                            </Field>
+                                        </div>
+                                        <div>
+                                            <Field name="address">
+                                                {({ input, meta }) => (
+                                                    <div>
+                                                        <label htmlFor="address" className="block mb-2 text-sm font-medium text-gray-900">Адрес</label>
+                                                        <input {...input} type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" />
+                                                        {meta.error && meta.touched && <span className="text-sm text-red-500">{meta.error}</span>}
+                                                    </div>
+                                                )}
+                                            </Field>
+                                        </div>
+                                        <div>
+                                            <Field name="kpp">
+                                                {({ input, meta }) => (
+                                                    <div>
+                                                        <label htmlFor="kpp" className="block mb-2 text-sm font-medium text-gray-900">КПП</label>
+                                                        <input {...input} type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" />
+                                                        {meta.error && meta.touched && <span className="text-sm text-red-500">{meta.error}</span>}
+                                                    </div>
+                                                )}
+                                            </Field>
+                                        </div>
+                                        <div className="w-full flex items-center justify-between p-4 md:p-5 border-b rounded-t">
+                                            <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Сохранить</button>
+                                            <button type="button" onClick={props.onCloseModal} className="text-white bg-red-500 hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Отменить</button>
+                                        </div>
+                                    </form>
+                                )
+                            }
+                        />
                     </div>
                 </div>
             </div>
